5381039ca4e97177d0e75dc7c90896a026f88b65
Shanel G <greenesh@zachevanscreative.com>
2023-02-04 16:03:06 -0500

    shamelessly added seminole styling

--- src/components/Prediction.js
+++ src/components/Prediction.js
@@ -2,15 +2,13 @@
 const Rank = ({rank}) => {
   return (
     <div className="poll">
-      <div className="current">
-        <p>Current Rank: {rank.current_poll}</p>
+        <p className="current">Current Rank: {rank.current_poll}</p>
         <div className="predicted">
           <div className="prediction">
         <p className="new-rank">{rank.predicted_poll}</p>
         </div>
         </div>
       </div>
-    </div>
   )
 }
 
